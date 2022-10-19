55dff12d8e94fb7748d1de29cccb91eec7aaac55
Jeremiah <1442943357@qq.com>
2022-10-19 22:48:11 +0800

    important! tweak eval code

--- SentEval/senteval/sts.py
+++ SentEval/senteval/sts.py
@@ -180,6 +180,70 @@ class STSBenchmarkEval(STSEval):
         sick_data['y'] = [float(s) for s in sick_data['y']]
         self.samples += sick_data['X_A'] + sick_data["X_B"]
         return (sick_data['X_A'], sick_data["X_B"], sick_data['y'])
+    def run(self, params, batcher):
+        results = {}
+        train_and_dev_scores = []
+        train_and_dev_gs_scores = []
+        all_sys_scores = []
+        all_gs_scores = []
+        for dataset in self.datasets:
+            sys_scores = []
+            input1, input2, gs_scores = self.data[dataset]
+            for ii in range(0, len(gs_scores), params.batch_size):
+                batch1 = input1[ii:ii + params.batch_size]
+                batch2 = input2[ii:ii + params.batch_size]
+
+                # we assume get_batch already throws out the faulty ones
+                if len(batch1) == len(batch2) and len(batch1) > 0:
+                    enc1 = batcher(params, batch1)
+                    enc2 = batcher(params, batch2)
+
+                    for kk in range(enc2.shape[0]):
+                        sys_score = self.similarity(enc1[kk], enc2[kk])
+                        sys_scores.append(sys_score)
+            if dataset in ["train", "dev"]:
+                train_and_dev_scores.extend(sys_scores)
+                train_and_dev_gs_scores.extend(gs_scores)
+            all_sys_scores.extend(sys_scores)
+            all_gs_scores.extend(gs_scores)
+            results[dataset] = {'pearson': pearsonr(sys_scores, gs_scores),
+                                'spearman': spearmanr(sys_scores, gs_scores),
+                                'nsamples': len(sys_scores)}
+            logging.debug('%s : pearson = %.4f, spearman = %.4f' %
+                          (dataset, results[dataset]['pearson'][0],
+                           results[dataset]['spearman'][0]))
+
+        weights = [results[dset]['nsamples'] for dset in results.keys()]
+        list_prs = np.array([results[dset]['pearson'][0] for
+                            dset in results.keys()])
+        list_spr = np.array([results[dset]['spearman'][0] for
+                            dset in results.keys()])
+
+        avg_pearson = np.average(list_prs)
+        avg_spearman = np.average(list_spr)
+        wavg_pearson = np.average(list_prs, weights=weights)
+        wavg_spearman = np.average(list_spr, weights=weights)
+        all_pearson = pearsonr(all_sys_scores, all_gs_scores)
+        all_spearman = spearmanr(all_sys_scores, all_gs_scores)
+        train_and_dev_pearson = pearsonr(train_and_dev_scores, train_and_dev_gs_scores)
+        train_and_dev_spearman = spearmanr(train_and_dev_scores, train_and_dev_gs_scores)
+        results['all'] = {'pearson': {'all': all_pearson[0],
+                                      'mean': avg_pearson,
+                                      'wmean': wavg_pearson},
+                          'spearman': {'all': all_spearman[0],
+                                       'mean': avg_spearman,
+                                       'wmean': wavg_spearman}}
+        results["train_and_dev"] = { "pearson" : {"all": train_and_dev_pearson[0]},
+                                     "spearman" : {"all": train_and_dev_spearman[0]}
+        }
+        logging.debug('ALL : Pearson = %.4f, \
+            Spearman = %.4f' % (all_pearson[0], all_spearman[0]))
+        logging.debug('ALL (weighted average) : Pearson = %.4f, \
+            Spearman = %.4f' % (wavg_pearson, wavg_spearman))
+        logging.debug('ALL (average) : Pearson = %.4f, \
+            Spearman = %.4f\n' % (avg_pearson, avg_spearman))
+
+        return results
 
 class STSBenchmarkFinetune(SICKEval):
     def __init__(self, task_path, seed=1111):
@@ -202,7 +266,7 @@ class STSBenchmarkFinetune(SICKEval):
         sick_data['y'] = [float(s) for s in sick_data['y']]
         return sick_data
         
-class SICKRelatednessEval(STSEval):
+class SICKRelatednessEval(STSBenchmarkEval):
     def __init__(self, task_path, seed=1111):
         logging.debug('\n\n***** Transfer task : SICKRelatedness*****\n\n')
         self.seed = seed
